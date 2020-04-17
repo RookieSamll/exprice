38563a5ed8e01ec20cdb34a367fab8a0ded97679
RookieSamll <1925751630@qq.com>
2020-04-17 16:13:23 +0800

    提交

--- dome.js
+++ dome.js
@@ -1,13 +1,3 @@
-let {URL}=require("url");
-let cc=require("url")
+let path=require("path")
 
-let aa=new URL("https://www.baidu.com/s?ie=UTF-8&wd=node");
-let bb=aa.searchParams
-
-console.log(bb.get("ie"));
-let xx=cc.parse("https://www.baidu.com/s?ie=UTF-8&wd=node")
-console.log(xx)
-
-
-
-
+console.log(path.delimiter)
